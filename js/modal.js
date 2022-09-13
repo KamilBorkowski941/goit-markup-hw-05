fc8290af3fada108550c652740483c383a61faad
KamilBorkowski941 <kamil.borkowski941@gmail.com>
2022-09-13 03:53:17 +0200

    fixed popup

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,14 @@
-const openModalButtons: document.querySelector("[data-modal-target]"),
-onnst closeModalButtons: document.query(Selector("[data-close-button]"),
-const overlay = document.getElementById('overlay')
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
- openModalButtons.forEach(buttons => {
-buttons,addEventListener('click', () => {
-  const modal = document.querySelector(button.dataset.modalTarget)
-  openmodal(modal)
-})
- })
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
- function openmodal(modal)
- if (modal==null) return
- modal.classList.add('active')
-overlay.classList.add('active')
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
